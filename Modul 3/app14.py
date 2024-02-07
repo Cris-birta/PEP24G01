dfffa6e87efcc9c51e51a6594b90671fe3c607ee
Cris-birta <crisbirta@gmial.com>
2024-02-07 21:13:48 +0200

    Modulul 3

---
+++ Modul 3/app14.py
@@ -0,0 +1,20 @@
+parola = 7788
+
+# for incercare in range(100):
+#     parola_intodusa = int(input('Introduceti parola: '))
+#     if parola == parola_intodusa:
+#         print('acces granted')
+#         break
+#     else:
+#         print('parola gresita')
+
+
+
+
+while True:
+    parola_intodusa = int(input('Introduceti parola: '))
+    if parola == parola_intodusa:
+        print('acces granted')
+        break
+    else:
+        print('parola gresita')
