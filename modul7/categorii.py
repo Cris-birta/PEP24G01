613e12f0ade7115a29c733c1191e73268db97a0f
Cris-birta <crisbirta@gmial.com>
2024-03-06 21:00:39 +0200

    Modulul7

---
+++ modul7/categorii.py
@@ -0,0 +1,21 @@
+class Categorii():
+    def __init__(self, nume, pret, stoc):
+        self.nume = nume
+        self.pret = pret
+        self.stoc = stoc
+
+class Haine(Categorii):
+    pass
+
+class Accesorii (Categorii):
+    pass
+
+class Incaltaminte(Categorii):
+    pass
+
+# tricouri = Haine('tricou', 20, 35)
+
+
+
+
+
